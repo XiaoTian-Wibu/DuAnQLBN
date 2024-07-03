08f14dc547e9571d2eca269f3df2ac57c9f448f6
XiaoTian-Wibu <tieuthientd@gmail.com>
2024-07-03 22:03:44 +0700

    new file

---
+++ src/duan/qlbn/utils/SendMail.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package duan.qlbn.utils;
+
+/**
+ *
+ * @author HP
+ */
+public class SendMail {
+    private void guiMail(){
+        
+    }
+}
